41c34abe559b7393cb016866288d7bd9a2c4ffb6
Jerome Reduta <JRReduta@yahoo.com>
2023-04-28 13:38:52 -0700

    Finished 4-4

--- src/decision_structures/GradeMessage.java
+++ src/decision_structures/GradeMessage.java
@@ -1,4 +1,34 @@
 package decision_structures;
 
+import java.util.Scanner;
+
 public class GradeMessage {
+    public static void main(String[] args) {
+        System.out.println("Enter letter grade: ");
+        Scanner scan = new Scanner(System.in);
+        String grade = scan.next();
+        scan.close();
+        String message;
+        switch(grade) {
+            case "A":
+                message = "AMAZING";
+                break;
+            case "B":
+                message = "Nice";
+                break;
+            case "C":
+                message = "OKAY!";
+                break;
+            case "D":
+                message = "Do better";
+                break;
+            case "F":
+                message = "failed";
+                break;
+            default:
+                message = "Error";
+                break;
+        }
+        System.out.println(message);
+    }
 }
