007e260e39c9cadf7ad114ffddca16baf0a7608a
Jerome Reduta <JRReduta@yahoo.com>
2023-04-28 12:21:16 -0700

    Finished 3-5

--- src/gross_calculator/GrossPayCalculator.java
+++ src/gross_calculator/GrossPayCalculator.java
@@ -1,10 +1,18 @@
 package gross_calculator;
 
+import java.util.Scanner;
+
 public class GrossPayCalculator {
 
     public static void main(String[] args){
-        int hours = 40;
-        double payRate = 25.50;
+        int hours = 0;
+        System.out.println("How many hours did you work?");
+        Scanner scan = new Scanner(System.in);
+        hours = scan.nextInt();
+        double payRate = 0;
+        System.out.println("What's your pay rate?");
+        payRate = scan.nextDouble();
+        scan.close();
         double grossPay = hours * payRate;
         System.out.println("Gross pay: " + grossPay);
     }
